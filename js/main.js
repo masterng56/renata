8096c00f8e02eb8b119712dc3bf4d9389a6c433a
masterng <masterng@yandex.ru>
2021-04-01 23:16:01 +0500

    Доделал первый блок в Ценах и услугах

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// открыть закрыть меню
+// var button = document.querySelector(".accordion-title");
+// var menu = document.querySelector(".top_nav_list");
+
+// button.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     button.classList.toggle("active")
+
+
+// });
